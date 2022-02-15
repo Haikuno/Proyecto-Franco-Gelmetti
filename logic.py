61e56cc4b3226e466db605018618b4a98b3a9d2e
Haikuno <mopprower@gmail.com>
2022-02-14 23:12:05 -0300

    primer commit

---
+++ logic.py
@@ -0,0 +1,81 @@
+import pandas as pd
+
+class Producto():
+    def __init__(self, nombre, categoria, automatizable):
+        self.nombre = nombre
+        self.categoria = categoria
+        self.automatizable = automatizable
+    ubicacion = None
+
+class Ubicacion():
+    def __init__(self, id, modulo, automatizable):
+        self.id = id
+        self.modulo = modulo
+        self.automatizable = automatizable
+class Modulo():
+    def __init__(self, id):
+        self.id = id
+    categoria = None
+
+def computar():
+
+    df_productos = pd.read_excel('listagordo.xlsx') # Guardar en DataFrame el input de productos
+    df_productos = df_productos.sort_values(["Demanda"], ascending=False) # Sortear por demanda por las dudas
+
+    df_lista = pd.read_excel('listaprio.xlsx') # Lista de ubicaciones
+    df_lista = df_lista.sort_values(["Prioridad"], ascending=True)
+
+    productos = []
+    ubicaciones = []
+    modulos = []
+    modulos_ocupados = [] # ID
+    output = []
+
+    for row in df_productos.itertuples():
+        productos.append(Producto(row.Nombre, row.Categoría, row.Automatizable))
+
+    for row in df_lista.itertuples():
+        if str(row.Ubicación).startswith('A'):
+            ubicaciones.append(Ubicacion(row.Ubicación, row.Módulo, True))
+        else:
+            ubicaciones.append(Ubicacion(row.Ubicación, row.Módulo, False))
+
+        modulo = Modulo(row.Módulo)
+        if len(modulos) == 0:
+            modulos.append(modulo)
+
+        if modulo.id not in modulos_ocupados:
+            modulos.append(modulo)
+            modulos_ocupados.append(modulo.id)
+
+    for producto in productos:
+
+        for ubicacion in ubicaciones:
+            if producto.ubicacion is not None:
+                break
+            for modulo in modulos:
+                if ubicacion.modulo == modulo.id:
+                    if producto.automatizable == False:
+                        if ubicacion.automatizable == True:
+                            continue
+                    if modulo.categoria is None or modulo.categoria == producto.categoria:
+                        modulo.categoria = producto.categoria
+                        producto.ubicacion = ubicacion.id
+                        del(ubicaciones[ubicaciones.index(ubicacion)])
+                        break
+
+    for producto in productos:
+        output.append({
+            'Nombre': producto.nombre,
+            'Ubicación': producto.ubicacion,
+            'Categoria': producto.categoria,
+            'Automatizable' : producto.automatizable,
+            })
+
+    #--------------------------------------------------------------------------------#
+
+    output = pd.DataFrame(output)
+
+    #TODO:cambiar pathing dependiendo de pc
+
+    output.to_excel(r'/home/agu/Code/gelme/Proyecto-Franco-Gelmetti/Output.xlsx', index = False) # Archivo final
--- main.py
+++ main.py
@@ -1,84 +1,47 @@
-import pandas as pd
+import pygame
+import sys
+from pygame.locals import *
+import logic
+import button
 
-df_productos = pd.read_excel('listagordo.xlsx') # Guardar en DataFrame el input de productos
-df_productos = df_productos.sort_values(["Demanda"], ascending=False) # Sortear por demanda por las dudas
+# Inicializar pygame
+pygame.init()
+mainClock = pygame.time.Clock()
 
-df_lista = pd.read_excel('listaprio.xlsx') # Lista de ubicaciones
-df_lista = df_lista.sort_values(["Prioridad"], ascending=True)
+# Inicializar ventana
+ANCHO = 640
+ALTO = 480
+windowSurface = pygame.display.set_mode(
+    (ANCHO, ALTO),
+    0, 32, 0, 1
+)
+pygame.display.set_caption('Categorizador')
 
-class Producto():
-    def __init__(self, nombre, categoria, automatizable):
-        self.nombre = nombre
-        self.categoria = categoria
-        self.automatizable = automatizable
-    ubicacion = None
-    prioridad = None
+# Colores
+NEGRO = (0, 0, 0)
+BLANCO = (255, 255, 255)
 
-class Ubicacion():
-    def __init__(self, id, modulo, prioridad, automatizable):
-        self.id = id
-        self.modulo = modulo
-        self.prioridad = prioridad
-        self.automatizable = automatizable
-class Modulo():
-    def __init__(self, id):
-        self.id = id
-    categoria = None
+#load button images
+start_img = pygame.image.load('start_btn.png').convert_alpha()
+exit_img = pygame.image.load('exit_btn.png').convert_alpha()
 
+#create button instances
+start_button = button.Button(100, 200, start_img, 0.8) # X, Y, Imagen, Escala
+exit_button = button.Button(450, 200, exit_img, 0.8) # X, Y, Imagen, Escala
 
-productos = []
-ubicaciones = []
-modulos = []
-modulos_ocupados = [] # ID
-output = []
+while True:
+    # Events
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
 
-for row in df_productos.itertuples():
-    productos.append(Producto(row.Nombre, row.Categoría, row.Automatizable))
-
-for row in df_lista.itertuples():
-    if str(row.Ubicación).startswith('A'):
-        ubicaciones.append(Ubicacion(row.Ubicación, row.Módulo, row.Prioridad, True))
-    else:
-        ubicaciones.append(Ubicacion(row.Ubicación, row.Módulo, row.Prioridad, False))
-
-    modulo = Modulo(row.Módulo)
-    if len(modulos) == 0:
-        modulos.append(modulo)
-
-    if modulo.id not in modulos_ocupados:
-        modulos.append(modulo)
-        modulos_ocupados.append(modulo.id)
-
-for producto in productos:
-
-    for ubicacion in ubicaciones:
-        if producto.ubicacion is not None:
-            break
-        for modulo in modulos:
-            if ubicacion.modulo == modulo.id:
-                if producto.automatizable == False:
-                    if ubicacion.automatizable == True:
-                        continue
-                if modulo.categoria is None or modulo.categoria == producto.categoria:
-                    modulo.categoria = producto.categoria
-                    producto.ubicacion = ubicacion.id
-                    producto.prioridad = ubicacion.prioridad
-                    del(ubicaciones[ubicaciones.index(ubicacion)])
-                    break
-
-for producto in productos:
-    output.append({
-        'Nombre': producto.nombre,
-        'Ubicación': producto.ubicacion,
-        'Categoria': producto.categoria,
-        'prio en top': producto.prioridad,
-        'Automatizable' : producto.automatizable,
-        })
-
-#--------------------------------------------------------------------------------#
-
-output = pd.DataFrame(output)
-
-#TODO:cambiar pathing dependiendo de pc
-
-output.to_excel(r'/home/agu/Code/gelme/Proyecto-Franco-Gelmetti/Output.xlsx', index = False) # Archivo final
+    # Draw
+    windowSurface.fill(BLANCO)
+    if start_button.draw(windowSurface):
+        logic.computar()
+    if exit_button.draw(windowSurface):
+        pygame.quit()
+        sys.exit()
+    pygame.display.update()
+    mainClock.tick(10)
